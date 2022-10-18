eba9e2d5d8d3b126d93e692bfb0a1f3b34b98aab
george827 <kinyanjuigeorge827@gmail.com>
2022-10-18 11:48:46 +0300

    fixed menu bar

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const hamburger=document.querySelector(".hamburger");
 const navmenu=document.querySelector(".nav-menu");
+const nav =document.querySelector(".navbar");
+const mainHead =document.querySelector(".main-header");
+
 
 hamburger.addEventListener("click",()=>{
     hamburger.classList.toggle("active");
     navmenu.classList.toggle("active");
+    nav.classList.toggle("active");
+    mainHead.classList.toggle("active");
 
 })
 
 document.querySelectorAll(".nav-link").forEach(n=> n.addEventListener("click",()=>{
     hamburger.classList.remove("active");
     navmenu.classList.remove("active");
+    nav.classList.remove("active");
+    mainHead.classList.remove("active");
 
 }))
